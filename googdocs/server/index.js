14ae8b83f4276c7ce37527b0a912acd82eda2d38
jinheng0301 <95295108+jinheng0301@users.noreply.github.com>
2024-05-10 23:13:59 +0800

    create connection with the mongoDB database

--- googdocs/server/index.js
+++ googdocs/server/index.js
@@ -5,9 +5,14 @@ const PORT = process.env.PORT | 3001;
 
 const app = express();
 
+const DB = 'mongodb+srv://jinheng:FPac!vA3Qf.zXZB@cluster0.slq3oxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(DB).then(() => {
+    console.log('connection successsful');
+}).catch((err) => {
+    console.log(err);
+})
+
 app.listen(PORT, '0.0.0.0', function () {
-    console.log(`connected at port ${PORT}`);
-    console.log(`another one line`);
     console.log(`connected at port ${PORT}`);
     console.log(`connected at port ${PORT}`);
     // backtick `` is present of the escape key
